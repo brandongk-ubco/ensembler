Fixing dataset statistics calculation.

ensembler/Model.py
@@ -38,17 +38,9 @@ class Segmenter(pl.LightningModule):
         parser.add_argument('--encoder_name',
                             type=str,
                             default="efficientnet-b0")
-        parser.add_argument('--batch_size', type=int, default=4)
-        parser.add_argument('--num_workers',
-                            type=int,
-                            default=os.cpu_count() // 2)
         parser.add_argument('--patience', type=int, default=10)
         parser.add_argument('--depth', type=int, default=5)
-        parser.add_argument('--data_dir',
-                            type=str,
-                            nargs='?',
-                            const=os.environ.get("DATA_DIR", None),
-                            default=os.environ.get("DATA_DIR", None))
+
         return parser
 
     def get_model(self):
@@ -90,13 +82,6 @@ class Segmenter(pl.LightningModule):
 
     def get_optimizer(self):
         return torch.optim.Adam
-
-    def all_dataloader(self):
-        return torch.utils.data.DataLoader(self.all_data,
-                                           batch_size=self.batch_size,
-                                           num_workers=self.num_workers,
-                                           shuffle=False,
-                                           drop_last=False)
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(self.train_data,

ensembler/aggregators/__init__.py
@@ -32,11 +32,10 @@ def batch_loss(y_hat,
             assert torch.all(y_batch[2, :, :, :] - y_batch[0, :, :, :] <= eps)
             assert torch.all(y_batch[3, :, :, :] - y_batch[0, :, :, :] <= eps)
         except AssertionError:
-            print(torch.sum(y_batch[0, :, :, :] - y_batch[1, :, :, :]))
-            print(torch.sum(y_batch[0, :, :, :] - y_batch[2, :, :, :]))
-            print(torch.sum(y_batch[0, :, :, :] - y_batch[3, :, :, :]))
-            import pdb
-            pdb.set_trace()
+            print("Warning: Batch masks are not equal!")
+            print(torch.max(y_batch[0, :, :, :] - y_batch[1, :, :, :]))
+            print(torch.max(y_batch[0, :, :, :] - y_batch[2, :, :, :]))
+            print(torch.max(y_batch[0, :, :, :] - y_batch[3, :, :, :]))
 
         y_hat_batch = reduction(y_hat_batch)
         y_batch = y_batch[0, :, :, :]

ensembler/dataset_statistics.py
@@ -1,11 +1,21 @@
-import matplotlib
 import os
 from tqdm import tqdm
 import pandas as pd
 import json
 from ensembler.datasets import split_dataset, Datasets
-from ensembler.Model import Segmenter as model
 from ensembler.augments import get_augments
+import torch
+
+description = "Prepare dataset statistics required for sampling."
+
+
+def add_argparse_args(parser):
+    parser.add_argument('--data_dir',
+                        type=str,
+                        nargs='?',
+                        const=os.environ.get("DATA_DIR", None),
+                        default=os.environ.get("DATA_DIR", None))
+    parser.add_argument('--num_workers', type=int, default=os.cpu_count() // 2)
 
 
 def execute(args):
@@ -15,15 +25,20 @@ def execute(args):
     outdir = os.path.join(dict_args["data_dir"], dict_args["dataset"])
 
     dataset = Datasets.get(dict_args["dataset"])
+    train_data, val_data, test_data, all_data = dataset.get_dataloaders(
+        os.path.join(dict_args["data_dir"], dict_args["dataset"]),
+        get_augments(dataset.image_height, dataset.image_width))
 
-    m = model(get_augments, **dict_args)
-
-    dataloader = m.all_dataloader()
+    dataloader = torch.utils.data.DataLoader(
+        all_data,
+        batch_size=1,
+        num_workers=dict_args["num_workers"],
+        shuffle=False,
+        drop_last=False)
 
     image_names = dataloader.dataset.get_image_names()
     results = pd.DataFrame(columns=["sample"] + list(dataset.classes),
                            index=range(len(image_names)))
-    batch_size = dict_args["batch_size"]
 
     statistics_file = os.path.join(outdir, "class_samples.csv")
 
@@ -38,9 +53,8 @@ def execute(args):
                 pixels = img.size()[1] * img.size()[2]
                 class_counts = mask.sum(axis=0).sum(axis=0).numpy() / pixels
                 img_results = dict(zip(dataset.classes, class_counts))
-                results.iloc[batch * batch_size +
-                             i] = [image_names[batch * batch_size + i]] + list(
-                                 img_results.values())
+                results.iloc[batch + i] = [image_names[batch + i]] + list(
+                    img_results.values())
         assert not results.isna().any(axis=None)
         results.to_csv(statistics_file, index=False)
 

ensembler/datasets/voc.py
@@ -6,24 +6,20 @@ from PIL import Image
 import os
 from ensembler.datasets.AugmentedDataset import DatasetAugmenter
 
+classes = [
+    "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus",
+    "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike",
+    "person", "potted plant", "sheep", "sofa", "train", "tv/monitor"
+]
+
 image_height = 512
 image_width = 512
-num_classes = 21
+num_classes = len(classes)
 batch_size = 8
 loss_weights = [1.] * num_classes
 
-voc_folder = "/mnt/d/work/datasets/voc"
-
 
 class VOCDataset(Dataset):
-
-    classes = [
-        "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus",
-        "car", "cat", "chair", "cow", "diningtable", "dog", "horse",
-        "motorbike", "person", "potted plant", "sheep", "sofa", "train",
-        "tv/monitor"
-    ]
-
     def __init__(self, voc_folder, val_percent=5., split="train"):
 
         self.split = split
@@ -37,6 +33,11 @@ class VOCDataset(Dataset):
             self.data = torchvision.datasets.VOCSegmentation(
                 voc_folder,
                 image_set='val',
+                download=not os.path.exists(voc_folder))
+        elif self.split == "all":
+            self.images = self.data = torchvision.datasets.VOCSegmentation(
+                voc_folder,
+                image_set='trainval',
                 download=not os.path.exists(voc_folder))
         elif split == "test":
             raise ValueError("Test split not implemented.")
@@ -61,11 +62,10 @@ class VOCDataset(Dataset):
         mask = Image.open(mask_path)
         mask = np.array(mask)
         mask[mask == 255] = 0
-        label_mask = np.zeros(
-            (len(self.classes), mask.shape[0], mask.shape[1]),
-            dtype=np.float32)
+        label_mask = np.zeros((num_classes, mask.shape[0], mask.shape[1]),
+                              dtype=np.float32)
 
-        for k, v in enumerate(self.classes):
+        for k, v in enumerate(classes):
             label_mask[k, mask == k] = 1
 
         label_mask = np.round(label_mask, 0)
@@ -89,10 +89,11 @@ def get_dataloaders(voc_folder, augmentations):
     train_data = VOCDataset(voc_folder, split="train")
     val_data = VOCDataset(voc_folder, split="val")
     test_data = VOCDataset(voc_folder, split="val")
+    all_data = VOCDataset(voc_folder, split="all")
 
     train_transform, val_transform, test_transform = augmentations
     train_data = DatasetAugmenter(train_data, train_transform)
     val_data = DatasetAugmenter(val_data, val_transform)
     test_data = DatasetAugmenter(test_data, test_transform)
 
-    return train_data, val_data, test_data
+    return train_data, val_data, test_data, all_data