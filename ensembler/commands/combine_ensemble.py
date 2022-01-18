43ac768056ec86096998187b68b50d27f65dcd92
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2022-01-18 12:51:45 -0800

    Committing.

--- ensembler/commands/combine_ensemble.py
+++ ensembler/commands/combine_ensemble.py
@@ -1,26 +1,52 @@
-from ensembler.Dataset import Dataset
-from ensembler.datasets import Datasets
 from argh import arg
 import os
-import numpy as np
-from ensembler.utils import classwise
-from ensembler.p_tqdm import p_uimap as mapper
 from functools import partial
 import pandas as pd
 from typing import List
 import hashlib
-import json
+
+from functools import partial
+from tqdm import tqdm
+
+tqdm.pandas()
 
 
-@arg('dataset', choices=Datasets.choices())
+def calculate_improvement(df, current_row):
+
+    ensemble_size = current_row["ensemble_size"]
+    image = current_row["image"]
+    clazz = current_row["class"]
+    if ensemble_size == 1:
+        return None
+
+    assert ensemble_size > 1
+    assert ensemble_size <= df["ensemble_size"].max()
+
+    previous_row = df[(df["image"] == image) & (df["class"] == clazz) &
+                      (df["ensemble_size"] == ensemble_size - 1)]
+
+    assert len(previous_row) == 1
+
+    previous_value = previous_row["value"]
+    value = current_row["value"]
+
+    improvement = (value - previous_value)
+
+    return improvement.iloc[0]
+
+
 @arg('job_hashes', nargs='+')
-def combine_ensemble(dataset: Datasets, base_dir: str, job_hashes: List[str]):
+def combine_ensemble(base_dir: str, job_hashes: List[str]):
 
     metrics_file = os.path.join(base_dir, "metrics.csv")
 
     print("Loading {}".format(metrics_file))
     metrics = pd.read_csv(metrics_file)
-    metrics = metrics[["image", "class", "threshold", "metric", "value"]]
+    metrics = metrics.rename(columns={"job_hash": "ensemble_hash"})
+    metrics = metrics[metrics["ensemble_hash"] == job_hashes[0]]
+    metrics = metrics[[
+        "image", "class", "threshold", "metric", "value", "ensemble_hash"
+    ]]
     metrics["ensemble_size"] = 1
 
     results = [metrics]
@@ -33,11 +59,24 @@ def combine_ensemble(dataset: Datasets, base_dir: str, job_hashes: List[str]):
         print("Loading {}".format(metrics_file))
         metrics = pd.read_csv(metrics_file)
         metrics["ensemble_size"] = len(hashes)
+        metrics["ensemble_hash"] = ensemble_hash
         results.append(metrics)
     job_hashes.sort()
     ensemble_hash = hashlib.md5("".join(job_hashes).encode()).hexdigest()
     outfile = os.path.join(base_dir, "ensembles", ensemble_hash,
                            "combined_metrics.csv")
+
     print("Writing {}".format(outfile))
     df = pd.concat(results)
     df.to_csv(outfile, index=False)
+
+    iou = df[df["metric"] == "iou"].copy(deep=True)
+
+    improvement_caluculator = partial(calculate_improvement, df=iou)
+
+    iou["improvement"] = iou.progress_apply(
+        lambda row: improvement_caluculator(current_row=row), axis=1)
+
+    outfile = os.path.join(base_dir, "ensembles", ensemble_hash,
+                           "improvement.csv")
+    iou.to_csv(outfile, index=False)
--- ensembler/commands/visualize_ensemble.py
+++ ensembler/commands/visualize_ensemble.py
@@ -40,3 +40,46 @@ def visualize_ensemble(base_dir: str, ensemble_hash: List[str]):
                                "{}.png".format(clazz))
         fig.savefig(outfile, dpi=300, bbox_inches='tight', pad_inches=0.5)
         plt.close()
+
+    print("Loading Improvement")
+    improvement_file = os.path.join(base_dir, "ensembles", ensemble_hash,
+                                    "improvement.csv")
+
+    improvement = pd.read_csv(improvement_file)
+    improvement = improvement.dropna()
+
+    print("Plotting Improvement")
+
+    plot = sns.boxplot(data=improvement,
+                       x='ensemble_size',
+                       y='improvement',
+                       color='lightblue')
+    fig = plot.get_figure()
+    ax = fig.get_axes()[0]
+
+    ax.set_xlabel("Ensemble Size")
+    ax.set_ylabel("mIoU Improvement over Previous")
+
+    ax.set_title("Improvement by Ensemble Size")
+
+    # plot.set_yticks(np.linspace(0, 1, num=11))
+
+    print("Saving Improvement plot")
+
+    outfile = os.path.join(base_dir, "ensembles", ensemble_hash,
+                           "improvement.png")
+    fig.savefig(outfile, dpi=300, bbox_inches='tight', pad_inches=0.5)
+    plt.close()
+
+    mIoU = improvement.groupby(by="ensemble_size").mean().sort_values(
+        by="ensemble_size", ascending=False).reset_index()
+    plot = sns.scatterplot(data=mIoU,
+                           x='ensemble_size',
+                           y='improvement',
+                           legend=False)
+    fig = plot.get_figure()
+
+    outfile = os.path.join(base_dir, "ensembles", ensemble_hash,
+                           "mean_improvement.png")
+    fig.savefig(outfile, dpi=300, bbox_inches='tight', pad_inches=0.5)
+    plt.close()
